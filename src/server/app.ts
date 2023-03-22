c08468561d612c0adf629011eaa335fb99883f24
Beaudry Chase <beauchase213@gmail.com>
2023-03-21 19:03:31 -0500

    server sends sun times

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,13 @@
 const express = require('express')
+const sunCalc = require('suncalc')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/time', (req, res) => {
+  var lat = req.query.lat
+  var long = req.query.long
+  var date = req.query.date
+  res.json(sunCalc.getTimes(date?? Date.now(), lat, long))
 })
 
 app.listen(port, () => {
