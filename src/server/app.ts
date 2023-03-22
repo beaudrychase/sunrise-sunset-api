af832d964853e26b8520d7e8e5b3cfb494d3cafd
Beaudry Chase <beauchase213@gmail.com>
2023-03-22 18:30:22 -0500

    Api validation (#2)
    * added validation and started unit testing
    
    * updated tests
    
    * added eslint rules and prettier config

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,15 +1,62 @@
-const express = require('express')
-const sunCalc = require('suncalc')
+import express = require('express')
+import { validationResult } from 'express-validator'
+import { checkSchema } from 'express-validator/src/middlewares/schema'
+import sunCalc = require('suncalc')
 const app = express()
-const port = 3000
 
-app.get('/time', (req, res) => {
-  var lat = req.query.lat
-  var long = req.query.long
-  var date = req.query.date
-  res.json(sunCalc.getTimes(date?? Date.now(), lat, long))
-})
+app.set('port', process.env.PORT || 3000)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get(
+  '/time',
+  checkSchema({
+    lat: {
+      in: ['params', 'query'],
+      isDecimal: {
+        bail: true,
+        errorMessage: 'Value must be decimal value',
+      },
+      custom: {
+        options: value => {
+          return -90 <= value && 90 >= value
+        },
+        errorMessage: 'Value must be valid latitude (-90 <= latitude <= 90)',
+      },
+    },
+    long: {
+      in: ['params', 'query'],
+      isDecimal: {
+        bail: true,
+        errorMessage: 'Value must be decimal value',
+      },
+      custom: {
+        options: value => {
+          return -90 <= value && 90 >= value
+        },
+        errorMessage:
+          'Value must be valid longitude (-180 <= longitude <= 180)',
+      },
+    },
+    date: {
+      optional: true,
+      in: ['params', 'query'],
+      isDate: {
+        errorMessage: 'Value must be a date',
+      },
+      customSanitizer: {
+        options: value => Date.parse(value),
+      },
+    },
+  }),
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const lat = req.query.lat
+    const long = req.query.long
+    const date = req.query.date ?? Date.now()
+    res.status(200).json(sunCalc.getTimes(date, lat, long))
+  }
+)
+
+export default app
