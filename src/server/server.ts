d90f32c74bd9e70df16b0cdc4f141f911e847a8e
Beaudry Chase <beauchase213@gmail.com>
2023-04-24 15:58:01 -0500

    Added unit tests, babel compiler, claudia for AWS lambda

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,7 @@
 import app from './app'
 
+app.set('port', process.env.PORT || 3000)
+
 const server = app.listen(app.get('port'), () => {
   console.log(
     'App is running on http://localhost:%d in %s mode',
